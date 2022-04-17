3c29a51a6dbd45677e33b3742d53bde506aa1331
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2022-04-17 10:27:27 +0700

    :zap: day 3 done

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
 import { ConnectWallet } from '@3rdweb/react'
+import { GetServerSideProps } from 'next'
+import { sanityClient, urlFor } from '../../sanity'
+import { Collection } from '../../typings'
+import Link from 'next/link'
 
-const NFTDropPage = () => {
+interface Props {
+  collection: Collection
+}
+
+const NFTDropPage = ({ collection }: Props) => {
   // Auth
   // const connectWithMetamask = useMetamask()
   // const address = useAddress()
@@ -16,16 +24,15 @@ const NFTDropPage = () => {
           <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2 transition duration-500 hover:scale-105">
             <img
               className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
-              src="https://i.imgur.com/byuqHA4.jpg"
+              src={urlFor(collection.previewImage).url()}
               alt="nft"
             />
           </div>
           <div className="space-y-2 p-5 text-center">
-            <h1 className="text-4xl font-bold text-white">Organix Cozmoz</h1>
-            <h2 className="text-xl text-gray-300">
-              What would outer space be like if it consisted of giant organic
-              organisms?
-            </h2>
+            <h1 className="text-4xl font-bold text-white">
+              {collection.nftCollectionName}
+            </h1>
+            <h2 className="text-xl text-gray-300">{collection.description}</h2>
           </div>
         </div>
       </div>
@@ -34,12 +41,14 @@ const NFTDropPage = () => {
       <div className="flex flex-1 flex-col p-12 lg:col-span-6">
         {/* Header */}
         <header className="flex items-center justify-between">
-          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
-            <span className="font-extrabold underline decoration-pink-500/50">
-              Async-Dime
-            </span>{' '}
-            NFT Marketplace
-          </h1>
+          <Link href="/">
+            <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+              <span className="font-extrabold underline decoration-pink-500/50">
+                Async-Dime
+              </span>{' '}
+              NFT Marketplace
+            </h1>
+          </Link>
 
           {/* <button
             onClick={() => (address ? disconnect() : connectWithMetamask())}
@@ -62,12 +71,12 @@ const NFTDropPage = () => {
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
           <img
             className="w-80 object-cover pb-10 lg:h-40"
-            src="https://i.imgur.com/tqYgsND.png"
+            src={urlFor(collection.mainImage).url()}
             alt="nft"
           />
 
           <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
-            Organix Cozmoz | NFT Drop
+            {collection.title}
           </h1>
 
           <p className="pt-2 text-xl text-green-500">13/ 21 NFTs Claimed</p>
@@ -83,3 +92,46 @@ const NFTDropPage = () => {
 }
 
 export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const query = `*[_type == "collection" && slug.current== $id][0]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+      asset
+    },
+    previewImage {
+      asset
+    },
+    slug {
+      current
+    },
+    creator-> {
+      _id,
+      name,
+      address,
+      slug {
+        current
+      }
+    }
+  }`
+
+  const collection = await sanityClient.fetch(query, {
+    id: params?.id,
+  })
+
+  if (!collection) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      collection,
+    },
+  }
+}
